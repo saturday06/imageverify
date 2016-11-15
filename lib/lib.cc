62cc751e3f57c2f99fbdd4b39a0443734ede3fb6
Isamu Mogi <isamu@leafytree.jp>
2016-11-16 03:33:02 +0900

    wip

--- lib/lib.cc
+++ lib/lib.cc
@@ -1,18 +1,96 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <jpeglib.h>
-#include <png.h>
-#include <string>
-#include <fribidi/fribidi.h>
-#include <ft2build.h>
-#include FT_BITMAP_H
-#include FT_FREETYPE_H
 #include <raqm.h>
 
 #include "imageverify.h"
 
-int image_verify() {
-	puts("Hello!");
-    return 0;
+int image_verify()
+{
+	int argc = 4;
+	char* argv[] = {
+			"prog",
+			"/Users/saturday06/Downloads/f/google-fonts/ufl/ubuntu/Ubuntu-Regular.ttf",
+			"Hello",
+			"-ltr"
+	};
+
+    const char *fontfile;
+    const char *text;
+    const char *direction;
+    int i;
+    int ret = 1;
+
+    raqm_t *rq = NULL;
+    raqm_glyph_t *glyphs = NULL;
+    size_t count;
+    raqm_direction_t dir;
+
+    FT_Library ft_library = NULL;
+    FT_Face face = NULL;
+    FT_Error ft_error;
+
+    if (argc < 4)
+    {
+        printf ("Usage:\n FONT_FILE TEXT DIRECTION\n");
+        return 1;
+    }
+
+    fontfile =  argv[1];
+    text = argv[2];
+    direction = argv[3];
+
+    ft_error = FT_Init_FreeType (&ft_library);
+    if (ft_error)
+        goto final;
+    ft_error = FT_New_Face (ft_library, fontfile, 0, &face);
+    if (ft_error)
+        goto final;
+    ft_error = FT_Set_Char_Size (face, face->units_per_EM, 0, 0, 0);
+    if (ft_error)
+        goto final;
+
+    dir = RAQM_DIRECTION_DEFAULT;
+    if (strcmp (direction, "-rtl") == 0)
+      dir = RAQM_DIRECTION_RTL;
+    else if (strcmp (direction, "-ltr") == 0)
+      dir = RAQM_DIRECTION_LTR;
+
+    rq = raqm_create ();
+    if (rq == NULL)
+        goto final;
+
+    if (!raqm_set_text_utf8 (rq, text, strlen (text)))
+        goto final;
+
+    if (!raqm_set_freetype_face (rq, face))
+        goto final;
+
+    if (!raqm_set_par_direction (rq, dir))
+        goto final;
+
+    if (!raqm_layout (rq))
+        goto final;
+
+    glyphs = raqm_get_glyphs (rq, &count);
+    if (glyphs == NULL)
+        goto final;
+
+    for (i = 0; i < count; i++)
+    {
+        printf ("%d %d %d %d %d %d\n",
+               glyphs[i].index,
+               glyphs[i].x_offset,
+               glyphs[i].y_offset,
+               glyphs[i].x_advance,
+               glyphs[i].y_advance,
+               glyphs[i].cluster);
+    }
+
+    ret = 0;
+
+final:
+    raqm_destroy (rq);
+    FT_Done_Face (face);
+    FT_Done_FreeType (ft_library);
+
+    return ret;
 }
